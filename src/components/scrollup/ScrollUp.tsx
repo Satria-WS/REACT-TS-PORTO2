bdfedf71dfb2787b8bf8e105bedf28cb4ef8573e
Satria-WS <extencezone02@gmail.com>
2023-06-11 11:11:18 +0700

    change scroll up to react

--- src/components/scrollup/ScrollUp.tsx
+++ src/components/scrollup/ScrollUp.tsx
@@ -3,23 +3,38 @@ import React, { useEffect, useState } from "react";
 import "./scrollup.css";
 
 const Scrollup = () => {
-  //   window.addEventListener("scroll", function () {
-  //     const scrollUp = document.querySelector(".scrollup");
-  //     //   if scroll higher than 500
-  //     if (this.scrollY >= 1000) scrollUp?.classList.add("show-scroll");
-  //     else scrollUp?.classList.remove("show-scroll");
-  //   });
+  // window.addEventListener("scroll", function () {
+  //   const scrollUp = document.querySelector(".scrollup");
+  //   //   if scroll higher than 500
+  //   if (this.scrollY >= 1000) scrollUp?.classList.add("show-scroll");
+  //   else scrollUp?.classList.remove("show-scroll");
+  // });
 
-  const [showScroll, setShowScroll] = useState<boolean>(false);
+  const [showScroll, setShowScroll] = useState(false);
 
-  const handleScroll = () => {
-    if (window.screenY >= 1000) {
-      setShowScroll(true);
-    }
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 1000) {
+        setShowScroll(true);
+      } else {
+        setShowScroll(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  // "scrollup"
   return (
-    <a href="#" className="scrollup">
-      <i className="uil uil-arrow-up scrollup__icon"></i>
+    <a href="#" className={`scrollup ${showScroll ? "show-scroll" : ""}`}>
+      <i onClick={scrollToTop} className="uil uil-arrow-up scrollup__icon"></i>
     </a>
   );
 };
