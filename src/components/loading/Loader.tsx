c5560bfc838276659829c55f75b0d3aa5657e84f
Satria-WS <extencezone02@gmail.com>
2023-07-16 14:42:43 +0700

    updated css loader

--- src/components/loading/Loader.tsx
+++ src/components/loading/Loader.tsx
@@ -2,10 +2,12 @@ import "./loader.css";
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <div className="outer"></div>
-      <div className="middle"></div>
-      <div className="inner"></div>
+    <div className="spinner">
+      <div className="loader">
+        <div className="outer"></div>
+        <div className="middle"></div>
+        <div className="inner"></div>
+      </div>
     </div>
   );
 };
